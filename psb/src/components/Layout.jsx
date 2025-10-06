fd6a0874e3c085ad35c38d42589c4351d3ae14e3
Panya <panya.rattanarom@sky.uk>
2025-10-06 10:31:46 +0100

    CREATE: layout

---
+++ psb/src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
