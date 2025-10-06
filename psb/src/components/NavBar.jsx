602970a2d7068cfcab86f3d1c7c2566e61000355
Panya <panya.rattanarom@sky.uk>
2025-10-06 12:19:06 +0100

    MODIFY: fix not responsive

--- psb/src/components/NavBar.jsx
+++ psb/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NavBar = () => {
   return (
     <div className="w-full h-24 p-5 border-gray shadow-lg">
-      <div className="w-full h-full grid grid-cols-10 content-center items-center ">
+      <div className="w-full h-full grid grid-cols-3 content-center items-center ">
         <div className="col-span-2 flex flex-row items-center gap-5">
           <a href="https://www.qa.com">
             <img src="../public/qaLogo.png" alt="QA Ltd"></img>
