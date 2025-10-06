0d017419f171eb21f1aab4cb51c583a09f135d92
bryanbarakatsky <bryan.barakat.work@gmail.com>
2025-10-06 12:18:23 +0100

    CREATE: creation of table todo list

--- psb/src/App.jsx
+++ psb/src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
+
 import "./App.css";
+import TodoPage from "./pages/TodoPage";
 
 function App() {
-  return <></>;
+  return <>
+    <TodoPage />
+  </>;
 }
 
 export default App;
