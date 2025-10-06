5677fe66df8e7fbc2f76d7f213d1df05a8ea6eeb
SaranyaaUK <saranyaajk95@gmail.com>
2025-10-06 15:13:06 +0100

    CHORE:Form update

--- psb/src/components/Form.jsx
+++ psb/src/components/Form.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { React, useState } from "react";
+import todoData from "../data/todoData.js"
 
-const handleSubmit = () => {
-    alert("Form submitted")
+
+// Check for useMemo usage - if we are reading the files and finding the length
+const handleSubmit = (e) => {
+    console.log(e.target.value)
+    alert("Todo completed")
 }
 
 const handleCompleted = () => {
@@ -9,36 +13,43 @@ const handleCompleted = () => {
 }
 
 const options = {
-  year: "numeric",
-  month: "numeric",
-  day: "numeric",
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric",
-  hour12: false,
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: false,
 };
 const datetime = new Intl.DateTimeFormat("en-US", options).format(Date.now())
 
 const Form = ({ }) => {
+
+    const [description, setDescription] = useState("Add Description");
+
+    const handleChange = (e) => {
+        setDescription(e.target.value)
+    }
+
     return (
-        <form className="p-4 grid grid-flow-col grid-rows-5 gap-4 flex">
+        <form className="w-p-4 grid grid-flow-col grid-rows-5 gap-4 flex" onSubmit={handleSubmit}>
             <h3 className="text-4xl text-left"> Add/Edit Todo</h3>
             <label>
                 Description:
-                <input className="m-4 border-2 border-solid" type="input" name="todoDescription" />
+                <input className="m-4 border-2 border-solid rounded-lg" type="input" name="todoDescription" value={description} onChange={(e) => handleChange(e)} />
             </label>
 
             <label>
                 {/* TO DO - Add logic to get the date when you open for edit*/}
-                {`Created on: ${datetime.replace(',','@')}`}
+                {`Created on: `} <span>{datetime.replace(',', '@')}</span>
             </label>
 
             <label>
-                <input className="m-2 p-4" type="checkbox" name="todoCompleted" onClick={handleCompleted}/>
+                <input className="m-2 p-4" type="checkbox" name="todoCompleted" onClick={handleCompleted} />
                 Completed
             </label>
 
-            <input className="bg-blue-500 w-24 p-4 rounded-lg" type="submit" value="Submit" onClick={handleSubmit} />
+            <input className="bg-blue-500 w-24 p-4 rounded-lg " type="submit" value="Submit" />
         </form>
     );
 };
