89094312434ca64f26787f816a6de16e8d3cb1ea
bryanbarakatsky <bryan.barakat.work@gmail.com>
2025-10-06 14:09:52 +0100

    CREATE: created table dynamic changes

--- psb/src/components/Layout.jsx
+++ psb/src/components/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
     <>
       <div className="flex flex-col min-h-screen">
         <NavBar />
-        <main className="flex-grow">{children}</main>
+        <main className="flex justify-center items-center flex-grow">{children}</main>
         <Footer />
       </div>
     </>
